8d3b4eface4867c95fff5211d117e96734dbe65e
DireTHMC <Dire_Straits@tut.by>
2022-05-22 19:01:16 +0300

    First home work

---
+++ FstHW.py
@@ -0,0 +1,13 @@
+'''
+ДЗ
+1. Создать переменные, которые содержат данные о: Вашем имени, любимом цвете, любимой группе и фильме
+2. Написать команду, которая выведет все это на экран
+3. Создать пустой репозиториц и добавить туда файл с задачей. Решение прислать ссылкой на репозиторий
+
+'''
+
+FirstAndSecondName = "My name: Anton Kim."
+FaviriteColor = "My favorite color is Black."
+FavoriteBand = "My favorite music band is Dire Straits."
+FavoriteMovie = "My favorite movie - 12 Chairs."
+print(FirstAndSecondName, FaviriteColor, FavoriteBand, FavoriteMovie)
